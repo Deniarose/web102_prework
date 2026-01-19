c95c793f689f55e151f4332f743c84b4b5dff9f5
Denia Rosiclair <drosiclair2022@fau.edu>
2026-01-18 22:31:52 -0500

    call addGamesToPage to display game cards

--- index.js
+++ index.js
@@ -59,6 +59,7 @@ function addGamesToPage(games) {
 
 
 }
+    addGamesToPage(GAMES_JSON);
 
 // call the function we just defined using the correct variable
 // later, we'll call this function using a different list of games
